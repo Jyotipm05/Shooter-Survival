fa794ee8d7b32c8fe845249598adf927e8d08112
dark_luminex <jyotip.m05@gmail.com>
2026-02-01 14:54:22 +0530

    Progressed

--- Assets/scripts/globalController.js
+++ Assets/scripts/globalController.js
@@ -3,6 +3,11 @@
 // Attach to a central scene object (e.g., World Object Controller).
 
 // @input bool startOnAwake = false
+// @input SceneObject triggers
+// @input SceneObject startBtn
+// @input SceneObject RestartBtn
+// @input Component.UIButton startButton
+// @input Component.UIButton restartButton
 
 // Initialize global flag
 if (global.gameRunning === undefined) {
@@ -10,21 +15,27 @@ if (global.gameRunning === undefined) {
 }
 
 // Control functions
-global.startGame = function() {
-    global.gameRunning = true;
-};
-
-global.stopGame = function() {
-    global.gameRunning = false;
-};
-
-global.toggleGame = function() {
+function startGame() {
+    script.triggers.enabled = global.gameRunning = true;
+    script.startBtn.enabled = false;
+    script.RestartBtn.enabled = false;
+}
+function stopGame() {
+    script.triggers.enabled = global.gameRunning = false;
+    script.RestartBtn.enabled = true;
+}
+function toggleGame() {
     global.gameRunning = !global.gameRunning;
-};
+    script.triggers.enabled = global.gameRunning;
+    script.RestartBtn.enabled = !global.gameRunning;
+}
 
-script.startGame = global.startGame;
-script.stopGame = global.stopGame;
-script.toggleGame = global.toggleGame;
+script.startButton.onClick = function() {
+    startGame();
+}
+script.restartButton.onClick = function() {
+    startGame();
+}
 
 // Optional: expose a way to start via a tap on this object if it has an InteractionComponent
 if (script.getSceneObject().getComponent("Component.InteractionComponent")) {
